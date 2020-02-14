be503452861b1632e905364d884b1bcb94415622
laxmi <laxmi@gmail.com>
2020-02-14 06:01:47 +0530

    assets path in production mode

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,5 @@
 class Reply < ApplicationRecord
+	# Associations
 	belongs_to :comment, optional: :true
 	belongs_to :user
 	has_many :nested_replys, class_name: "Reply", foreign_key: :reply_to
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Associations
   has_many :posts
   has_many :comments
   has_many :replies, class_name: "Reply"
+
+  # Validation
+	validates :name, presence: true
 end
