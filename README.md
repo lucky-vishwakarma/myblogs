46ad251ec184dc733f2294b8a35e8e0eb3c65fcd
laxmi <laxmi@gmail.com>
2020-02-14 10:53:30 +0530

    Add seed data

--- README.md
+++ README.md
@@ -6,17 +6,21 @@ application up and running.
 Things you may want to cover:
 
 * Ruby version
-
+	ruby '2.6.0'
 * System dependencies
+	Rails '5.2.3'
 
 * Configuration
 
 * Database creation
+	rails db:create
+	rails db:migrate
 
 * Database initialization
+	rake db:seed
 
 * How to run the test suite
-
+	No test cases
 * Services (job queues, cache servers, search engines, etc.)
 
 * Deployment instructions
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+img1 = File.open(Rails.root + "app/assets/images/blog1.jpeg")
+img2 = File.open(Rails.root + "app/assets/images/blog3.jpeg")
+img3 = File.open(Rails.root + "app/assets/images/blog4.jpeg")
+user = User.create(name: "TestUser", email: "testuser@gmail.com", password: "12345678")
+records = [
+	{title: "Natures", description: "Discover the world of natural wonder on your doorstep with this first book of nature! Beginning in spring", image: img1},
+	{title: "Envoirment", description: "World Environment Day is held each year on June 5. It is one of the principal vehicles through which the United Nations (UN) stimulates worldwide awareness of the environment and enhances political attention and action. ", image: img2},
+	{title: "Study", description: "The education system in Canada encompasses both publicly-funded and private schools", image: img3}
+]
+
+records.each do |data|
+	u = user.posts.new(data)
+	u.save
+end
